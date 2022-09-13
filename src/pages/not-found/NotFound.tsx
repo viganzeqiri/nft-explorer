441398d4397c5cc4599f89113b7604e728b3d277
viganzeqiri <viganzeqirii@gmail.com>
2022-09-13 22:34:40 +0200

    added styles to the not found component

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,9 +1,30 @@
 import { FunctionComponent } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Flex, Heading, Link } from "@chakra-ui/react";
+import { PageContainer } from "components";
 
-interface INotFoundProps {}
-
-const NotFound: FunctionComponent<INotFoundProps> = () => {
-  return <div>404</div>;
+const NotFound: FunctionComponent = () => {
+  return (
+    <PageContainer>
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        direction="column"
+        minH="80vh"
+      >
+        <Heading size="2xl" mb={2}>
+          Oopps...!
+        </Heading>
+        <Heading size="md">Something went wrong</Heading>
+        <Heading size="md">
+          Take me back{" "}
+          <Link as={RouterLink} color="brand.primary" to="/">
+            home.
+          </Link>
+        </Heading>
+      </Flex>
+    </PageContainer>
+  );
 };
 
 export { NotFound };
